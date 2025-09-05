f147dc897e7a12e9eb46dd25133c8d2850a5d51a
Bohemian <siamabdullah1100@gmail.com>
2025-09-05 22:47:49 +0600

    Admin creation

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,10 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const DB_URL =
   process.env.MONGO_URL || "mongodb://localhost:27017/bohemian-notes";
 export const PORT = process.env.PORT || 5000;
 export const SALT = process.env.SALT || 10;
 export const JWT_SECRET = process.env.JWT_SECRET || "secretjsonwebtoken";
+export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
